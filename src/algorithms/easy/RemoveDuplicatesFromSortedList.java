c56524b3f99b2e0197826d81ded124c205b15192
Farkhad <askfarz>
2024-12-22 20:07:20 +0300

    22dec24

---
+++ src/algorithms/easy/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,42 @@
+package algorithms.easy;
+
+public class RemoveDuplicatesFromSortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = head;
+        ListNode next = head.next;
+        while (next != null) {
+            if (prev.val == next.val) {
+                prev.next = next.next;
+            } else {
+                prev = prev.next;
+            }
+            next = next.next;
+        }
+        return head;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
