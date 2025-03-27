a65be206427b8653f3feac888c5abb178eb5f2b0
Farkhad <askarovfarkhad.96@gmail.com>
2025-03-27 19:31:52 +0300

    27mar25

--- src/algorithms/easy/TwoSum.java
+++ src/algorithms/easy/TwoSum.java
@@ -5,23 +5,7 @@ import java.util.Map;
 
 public class TwoSum {
 
-    public int[] twoSum(int[] nums, int target) {
-        if (nums.length == 2) {
-            return new int[] {0, 1};
-        }
-
-        Map<Integer, Integer> temp = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            Integer indexOfDiff = temp.get(target - nums[i]);
-            if (indexOfDiff != null) {
-                return new int[] {i, indexOfDiff};
-            }
-            temp.put(nums[i], i);
-        }
-        return new int[] {-1, -1};
-    }
-
-    public static int[] twoSum2(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         if (nums.length == 2) {
             return new int[] {0, 1};
         }
